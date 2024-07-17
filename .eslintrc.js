73e91a7c11b183f0ff66d0d9a5f865f076e930c8
Brian Powell <brian@powell.place>
2024-07-17 16:15:16 -0700

    Linting for JSON specific repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,15 @@
 const config = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
-    'plugin:playwright/recommended',
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['json'],
   ignorePatterns: ['node_modules', '.eslintrc.js'],
-  overrides: [
-    {
-      files: ['*.ts'],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
-      },
-    },
-  ],
   rules: {
-    'no-console': 'off',
-    'prefer-template': 'error',
     'eol-last': ['error', 'always'],
   },
 }
